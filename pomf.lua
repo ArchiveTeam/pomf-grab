226412c2b0f6d4aa36ab6baa156bdcfcca7bb310
Arkiver2 <Arkiver@hotmail.com>
2015-06-09 18:24:01 +0200

    pomf.lua

---
+++ pomf.lua
@@ -0,0 +1,41 @@
+local url_count = 0
+local tries = 0
+local item_type = os.getenv('item_type')
+local item_value = os.getenv('item_value')
+  
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  -- NEW for 2014: Slightly more verbose messages because people keep
+  -- complaining that it's not moving or not working
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. ".  \n")
+  io.stdout:flush()
+  
+  if status_code ~= 200 and status_code ~= 404 then
+    io.stdout:write("\nServer returned "..http_stat.statcode..". Sleeping.\n")
+    io.stdout:flush()
+
+    os.execute("sleep 10")
+    
+    tries = tries + 1
+
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      return wget.actions.ABORT
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
